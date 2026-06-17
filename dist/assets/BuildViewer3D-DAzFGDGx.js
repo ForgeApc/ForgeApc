16fef2b00db152b8e495cdf47d30e109ddc250f9
ForgeApc <Rayaan7777.the.best@GMAIL.COM>
2026-06-17 16:11:39 -0400

    fix: stack 4 hero menu buttons in a single column

--- dist/assets/BuildViewer3D-DAzFGDGx.js
+++ dist/assets/BuildViewer3D-DAzFGDGx.js
@@ -1,4 +1,4 @@
-import{r as vt,g as kx,R as Vx,j as Qe}from"./index-CFyconIw.js";/**
+import{r as vt,g as kx,R as Vx,j as Qe}from"./index-DXk1M5t_.js";/**
  * @license
  * Copyright 2010-2026 Three.js Authors
  * SPDX-License-Identifier: MIT
